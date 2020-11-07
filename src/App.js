362d576f5b5b98b17ed6ff5138ff2d11100216de
gilang-novrizal <gilnov.201196@gmail.com>
2020-11-07 21:20:16 +0700

    test case

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
   const [qrcode, setQrcode] = React.useState('')
   const [number, setNumber] = React.useState(0)
   const [message, setMessage] = React.useState('')
+  const [user, setUser] = React.useState({
+    name: 'Aldo',
+    number: 6285894676306,
+    status:'Accepted',
+    invoice: 'INVOICE/0019201920',
+    resi1: 'RESI-18556',
+    resi2: 1750253452,
+    total: 1500000
+  })
 
   // get QR Code for login
   const login = ()=>{
@@ -34,6 +43,86 @@ function App() {
     .then((res)=> console.log(res.data))
     .catch((err)=>console.log(err))
   }
+
+  // send notif order
+  const sendNotifOrder = ()=>{
+    const body ={
+      number: user.number + '@c.us',
+      name: user.name
+    }
+    Axios.post(URL + '/sendNotif/Order', body)
+    .then((res)=> console.log(res.data))
+    .catch((err)=> console.log(err))
+  }
+  const sendNotifProccess = ()=>{
+    const body ={
+      number: user.number + '@c.us',
+      name: user.name,
+      status: user.status
+    }
+    Axios.post(URL + '/sendNotif/Proccess', body)
+    .then((res)=> console.log(res.data))
+    .catch((err)=> console.log(err))
+  }
+  const sendNotifJalur = ()=>{
+    const body ={
+      number: user.number + '@c.us',
+      name: user.name,
+    }
+    Axios.post(URL + '/sendNotif/Jalur', body)
+    .then((res)=> console.log(res.data))
+    .catch((err)=> console.log(err))
+  }
+  const sendNotifInvoice = ()=>{
+    const body ={
+      number: user.number + '@c.us',
+      name: user.name,
+      invoice: user.invoice
+    }
+    Axios.post(URL + '/sendNotif/Invoice', body)
+    .then((res)=> console.log(res.data))
+    .catch((err)=> console.log(err))
+  }
+  const sendNotifImport = ()=>{
+    const body ={
+      number: user.number + '@c.us',
+      name: user.name,
+      resi: user.resi1
+    }
+    Axios.post(URL + '/sendNotif/Import', body)
+    .then((res)=> console.log(res.data))
+    .catch((err)=> console.log(err))
+  }
+  const sendNotifAddress = ()=>{
+    const body ={
+      number: user.number + '@c.us',
+      name: user.name
+    }
+    Axios.post(URL + '/sendNotif/Address', body)
+    .then((res)=> console.log(res.data))
+    .catch((err)=> console.log(err))
+  }
+  const sendNotifBill = ()=>{
+    const body ={
+      number: user.number + '@c.us',
+      name: user.name,
+      bill: user.total
+    }
+    Axios.post(URL + '/sendNotif/Bill', body)
+    .then((res)=> console.log(res.data))
+    .catch((err)=> console.log(err))
+  }
+  const sendNotifOnDelivery = ()=>{
+    const body ={
+      number: user.number + '@c.us',
+      name: user.name,
+      resi: user.resi2
+    }
+    Axios.post(URL + '/sendNotif/OnDelivery', body)
+    .then((res)=> console.log(res.data))
+    .catch((err)=> console.log(err))
+  }
+
   console.log(qrcode)
   return (
     <>
@@ -48,6 +137,32 @@ function App() {
       <button onClick={()=>sendMessage()}>
         Send Message
       </button>
+      <div style={{display: 'flex', flexDirection: 'column',width: '100px' }}>
+        <button onClick={sendNotifOrder}>
+          Send notif 0.5A
+        </button>
+        <button onClick={sendNotifProccess}>
+          Send notif 0.5B
+        </button>
+        <button onClick={sendNotifJalur}>
+          Send notif 1
+        </button>
+        <button onClick={sendNotifInvoice}>
+          Send notif 2
+        </button>
+        <button onClick={sendNotifImport}>
+          Send notif 3
+        </button>
+        <button onClick={sendNotifAddress}>
+          Send notif 4
+        </button>
+        <button onClick={sendNotifBill}>
+          Send notif 5
+        </button>
+        <button onClick={sendNotifOnDelivery}>
+          Send notif 6
+        </button>
+      </div>
     </>
   );
 }
